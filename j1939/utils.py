dc3726ede264fc75cacfa5377e707e60217aa63c
Miller Lowe <Miller.Lowe@trailtech.net>
2018-08-30 09:04:05 -0700

    fix indention errors

--- j1939/utils.py
+++ j1939/utils.py
@@ -33,17 +33,17 @@ def set_mem_object(pointer, extension, value, channel='can0', bustype='socketcan
     close = False
 
     if sys.platform == 'win32':
-		if bus is None:
-	        bus = j1939.Bus(timeout=0.01, keygen=security.SeedToKey, broadcast=None, name='j1939StartGeneral', ignoreCanSendError=True)
-	        node = j1939.Node(bus, j1939.NodeName(), [src])
-    	    bus.connect(node)
-        	close = True
-	else:
-	    if bus is None:
-	        bus = j1939.Bus(channel=channel, bustype=bustype, timeout=0.01, keygen=security.SeedToKey, broadcast=False)
-	        node = j1939.Node(bus, j1939.NodeName(), [src])
-	        bus.connect(node)
-	        close = True
+        if bus is None:
+            bus = j1939.Bus(timeout=0.01, keygen=security.SeedToKey, broadcast=None, name='j1939StartGeneral', ignoreCanSendError=True)
+            node = j1939.Node(bus, j1939.NodeName(), [src])
+            bus.connect(node)
+            close = True
+    else:
+        if bus is None:
+            bus = j1939.Bus(channel=channel, bustype=bustype, timeout=0.01, keygen=security.SeedToKey, broadcast=False)
+            node = j1939.Node(bus, j1939.NodeName(), [src])
+            bus.connect(node)
+            close = True
 			
     dm14data = [length, 0x15, pointer, 0x00, 0x00, extension, 0xff, 0xff]
 
